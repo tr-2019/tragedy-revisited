b8a5b7095822edd6e562099fdc3ad1a818218c46
tr-2019 <58113051+tr-2019@users.noreply.github.com>
2020-01-06 17:19:25 +0100

    Update game.stages.js

--- game/game.stages.js
+++ game/game.stages.js
@@ -1,6 +1,6 @@
 /**
  * # Game stages definition file
- * Copyright(c) 2019 LeoAlexLennart <>
+ * Copyright(c) 2020 LeoAlexanderLennart <>
  * MIT Licensed
  *
  * Stages are defined using the stager API
@@ -12,11 +12,18 @@
 module.exports = function(stager, settings) {
 
      stager
-        .next('instructions')
-        .repeat('game', settings.REPEAT)
-        .next('end')
+        .stage('instructions')
+        .repeatStage('player', 6)
+        .step('pbgame')
+        .step('irgame1')
+        .step('irgame2')
+        .stage('end')
         .gameover();
 
     // Modify the stager to skip one stage.
     // stager.skip('instructions');
+
+    // To skip a step within a stage use:
+    // stager.skip('stageName', 'stepName');
+    // Notice: here all stages have just one step.
 };
