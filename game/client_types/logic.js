cc520a411aa48703a76fa0ff94ee168f67f30401
tr-2019 <58113051+tr-2019@users.noreply.github.com>
2020-01-08 20:02:25 +0100

    Update logic.js

--- game/client_types/logic.js
+++ game/client_types/logic.js
@@ -45,150 +45,152 @@ module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
     });
 
     stager.extendStep('pbgame_results', {
-            cb: function() {
-                var playerIds = [];
-                var p1Id, p2Id/**, p3Id*/;
-                var p1Choice, p2Choice/**, p3Choice*/;
-                var p1Payoff, p2Payoff/**, p3Payoff*/;
+        cb: function() {
+            var playerIds = [];
+            var p1Id, p2Id, p3Id;
+            var p1Choice, p2Choice, p3Choice;
+            var p1Payoff, p2Payoff, p3Payoff;
+            var i, len;
+            var sumPayoff;
+            var pid, choice, payoff;
+            
+            playerIds = Object.keys(node.game.history);
 
-                playerIds = Object.keys(node.game.history);
-                p1Id = playerIds[0];
-                p2Id = playerIds[1];
-                /**p3Id = playerIds[2];*/
-                p1Choice = getRecentChoice(p1Id, node.game.history);
-                p2Choice = getRecentChoice(p2Id, node.game.history);
-                /** p3Choice = getRecentChoice(p3Id, node.game.history);*/
+            sumPayoff = 0;
+            for (i = 0; i < playerIds.length; i++) {
 
-                if ('DEFECT' === p1Choice) {
-                    p1Payoff = 10;
+                pid = playerIds[i];
+                choice = getRecentChoice(pid, node.game.history);
+                if ('DEFECT' === choice) {
+                    payoff = 10;
                 }
                 else {
-                    p1Payoff = 5;
+                    payoff = 5;
                 }
-                if ('DEFECT' === p2Choice) {
-                    p2Payoff = 10;
-                }
-                else {
-                    p2Payoff = 5;
-                }
-               /** if ('DEFECT' === p3Choice) {
-                    p3Payoff = 10;
-                }
-                else {
-                    p3Payoff = 5;
-                } */
-                addCoins(p1Id, p1Payoff, node.game.history);
-                addCoins(p2Id, p2Payoff, node.game.history);
-               /** addCoins(p3Id, p3Payoff, node.game.history); */
-                sendToClient(p1Id, p1Payoff, p2Payoff, p2Choice/**, p3Payoff, p3Choice*/);
-                sendToClient(p2Id, p2Payoff, p1Payoff, p1Choice/**, p3Payoff, p3Choice);
-                /** sendToClient(p3Id, p3Payoff, p2Payoff, p2Choice, p1Payoff, p1Choice); */
-                updateWin(p1Id, p1Payoff);
-                updateWin(p2Id, p2Payoff);
-               /** updateWin(p3Id, p3Payoff); */
-            }
-        });
+                sumPayoff += payoff;
+                addCoins(pid, payoff, node.game.history);
+                updateWin(pid, payoff);
 
-        stager.extendStep('irgame1', {
-              matcher: {
-                  roles: [ 'DONOR', 'RECEIVER' ],
-                  match: 'round_robin',
-                  cycle: 'mirror_invert',
-                  // sayPartner: false
-                  // skipBye: false,
+                // Will be executed after the for-loop is finished.
+                // This is an anonymous self-executing function.
+                // It is called "closure".
+                (function(pid, payoff) {
+                    setTimeout(function() {                    
+                        sendToClient(pid, payoff, sumPayoff);
+                    }, 100);                    
+                })(pid, payoff);
+            }            
+        }
+    });
 
-              },
-              cb: function() {
-                  node.once.data('done', function(msg) {
-                      var offer, observer;
-                      offer = msg.data.offer;
+    stager.extendStep('irgame1', {
+        matcher: {
+            // roles: [ 'DONOR', 'RECEIVER' ],
+            match: 'round_robin',
+            cycle: 'mirror_invert',
+            // sayPartner: false
+            // skipBye: false,
+        },
+        cb: function() {
 
-                      observer = node.game.matcher.getMatchFor(msg.from);
-                      // Send the decision to the other player.
-                      node.say('decision', observer, msg.data.offer);
+            // Creating a new object every round.
+            node.game.offers = [];
+            
+            node.on.data('done', function(msg) {
+                var offer, observer;
+                offer = msg.data.offer;
 
-                  });
-                  console.log('Game round: ' + node.player.stage.round);
-              }
-          });
+                observer = node.game.matcher.getMatchFor(msg.from);
 
-        stager.extendStep('irgame2', {
-            matcher: {
-                roles: [ 'DONOR', 'RECEIVER' ],
-                match: 'round_robin',
-                cycle: 'mirror_invert',
-                // sayPartner: false
-                // skipBye: false,
-
-            },
-            cb: function() {
-                node.once.data('done', function(msg) {
-                    var offer, observer;
-                    offer = msg.data.offer;
-
-                    observer = node.game.matcher.getMatchFor(msg.from);
-                    // Send the decision to the other player.
-                    node.say('decision', observer, msg.data.offer);
-
+                node.game.offers.push({
+                    donation: offer,
+                    receiver: observer,
+                    from: msg.from
                 });
-                console.log('Game round: ' + node.player.stage.round);
+                
+            });
+            console.log('Game round: ' + node.player.stage.round);
+        }
+    });
+
+    stager.extendStep('irgame2', {
+        // maybe matcher here too.
+        cb: function() {            
+            var i, len, receiver, data;
+            len = node.game.offers.length;
+            for (i=0 ; i < len ; i++) {
+                receiver = node.game.offers[i].receiver;
+                data = {
+                    from: node.game.offers[i].from,
+                    donation: node.game.offers[i].donation                    
+                };
+
+                if (treatmentName === 'XXX') {
+                    // data.history = history_you_saved;
+                    // history[id].choices
+                }                
+                
+                // Send the decision to each player.
+                node.say('decision', receiver, data);
             }
+            
+            console.log('Game round: ' + node.player.stage.round);
+        }
+    });
+
+    stager.extendStep('end', {
+        cb: function() {
+            // Save data in the data/roomXXX directory.
+            node.game.memory.save('data.json');
+        }
+    });
+
+    stager.setOnGameOver(function() {
+        // Something to do.
+    });
+
+    function sendToClient(id, myPayoff, totalPayoff) {
+        node.say("pbgame_results", id, {
+            myEarning: myPayoff,
+            totalFish: totalPayoff,
+            myBank: getBankTotal(id, node.game.history)
         });
+        //node.say("myEarning", id, myPayoff);
+        //node.say("otherEarning", id, otherPayoff);
+        //node.say("myBank", id, getBankTotal(id, node.game.history));
+    }
+    
+    function updateWin(id, win) {
+        var client;
+        client = channel.registry.getClient(id);
+        client.win = client.win ? client.win + win : win;
+    }
 
-        stager.extendStep('end', {
-            cb: function() {
-                // Save data in the data/roomXXX directory.
-                node.game.memory.save('data.json');
-            }
-        });
+    // retrieves the player's total number of coins
+    function getBankTotal(id, history) {
+        return history[id].coins;
+    }
 
-        stager.setOnGameOver(function() {
-            // Something to do.
-        });
+    // appends the player's choice to history data
+    function addToHistory(id, choice, history) {
+        if (!history[id]) {
+            history[id] = {};
+            history[id].coins = 0;
+            history[id].choices = [];
+        }
+        history[id].choices.push(choice);
+        console.log(id + choice);
+    }
 
-        function sendToClient(id, myPayoff, otherPayoff, choice) {
-                node.say("pbgame_results", id, {
-                    myEarning: myPayoff,
-                    totalFish: otherPayoff + myPayoff ,
-                    myBank: getBankTotal(id, node.game.history),
-                    otherChoice: choice
-                });
-                //node.say("myEarning", id, myPayoff);
-                //node.say("otherEarning", id, otherPayoff);
-                //node.say("myBank", id, getBankTotal(id, node.game.history));
-            }
+    // retrieves the player's most recent choice
+    function getRecentChoice(id, history) {
+        return history[id].choices[history[id].choices.length - 1];
+    }
 
-        function updateWin(id, win) {
-                var client;
-                client = channel.registry.getClient(id);
-                client.win = client.win ? client.win + win : win;
-            }
-
-            // retrieves the player's total number of coins
-            function getBankTotal(id, history) {
-                return history[id].coins;
-            }
-
-            // appends the player's choice to history data
-            function addToHistory(id, choice, history) {
-                if (!history[id]) {
-                    history[id] = {};
-                    history[id].coins = 0;
-                    history[id].choices = [];
-                }
-                history[id].choices.push(choice);
-                console.log(id + choice);
-            }
-
-            // retrieves the player's most recent choice
-            function getRecentChoice(id, history) {
-                return history[id].choices[history[id].choices.length - 1];
-            }
-
-            // increases player's coins by received Payoff
-            function addCoins(id, payOff, history) {
-                history[id].coins += payOff;
-            }
+    // increases player's coins by received Payoff
+    function addCoins(id, payOff, history) {
+        history[id].coins += payOff;
+    }
 
 };
 
