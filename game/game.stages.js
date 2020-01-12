76219861be95e00ab51b6d5f42fd1dc224239060
tr-2019 <58113051+tr-2019@users.noreply.github.com>
2020-01-12 21:17:44 +0100

    Update game.stages.js

--- game/game.stages.js
+++ game/game.stages.js
@@ -36,7 +36,9 @@ module.exports = function(stager, settings) {
     // Modify the stager to skip one stage.
     stager.skip('instructions');
     stager.skip('quiz');
-
+    stager.skip('feedback', 'feedback2')
+  //  stager.skip('player', 'pbgame_respond');
+  //  stager.skip('player', 'pbgame_results');
     // To skip a step within a stage use:
     // stager.skip('stageName', 'stepName');
     // Notice: here all stages have just one step.
