248d400cbc376c07e9a71db0025612d6c1f80ace
tr-2019 <58113051+tr-2019@users.noreply.github.com>
2020-01-19 23:32:25 +0100

    Update channel.settings.js

--- channel/channel.settings.js
+++ channel/channel.settings.js
@@ -28,7 +28,7 @@ module.exports = {
      *
      * Default: the name of the game, as found in the package.json file.
      */
-    // name: 'tragedy_revisited',
+    name: 'tragedy_revisited',
 
     /**
      * ## alias (string|array) Optional
